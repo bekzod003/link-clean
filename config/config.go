e958fe9ca23ec1f60e58c52c390fc8706097aabe
bekzod003 <begiobm@gmail.com>
2022-07-19 18:58:57 +0500

    config

--- config/config.go
+++ config/config.go
@@ -3,28 +3,35 @@ package config
 import (
 	"log"
 
-	"github.com/Netflix/go-env"
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
 	HTTP struct {
-		Port string `env:"PORT,required=true"`
+		Port string `env:"PORT" env-required:"true"`
 	}
 	ServiceName string `env:"SERVICE_NAME"`
-	Environment string `env:"ENVIRONMENT,default:development"`
+	Environment string `env:"ENVIRONMENT" env-default:"development"`
 
 	PostgreSQL struct {
-		Host     string `env:"POSTGRES_HOST,required=true"`
-		Port     int    `env:"POSTGRES_PORT,required=true"`
-		User     string `env:"POSTGRES_USER,required=true"`
-		Password string `env:"POSTGRES_PASSWORD,required=true"`
-		DBName   string `env:"POSTGRES_DB_NAME,required=true"`
+		Host     string `env:"POSTGRES_HOST" env-required:"true"`
+		Port     int    `env:"POSTGRES_PORT" env-required:"true"`
+		User     string `env:"POSTGRES_USER" env-required:"true"`
+		Password string `env:"POSTGRES_PASSWORD" env-required:"true"`
+		DBName   string `env:"POSTGRES_DB_NAME" env-required:"true"`
 	}
 }
 
 func GetConfig() *Config {
+	var err error
+
+	if err = godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	cfg := Config{}
-	if _, err := env.UnmarshalFromEnviron(&cfg); err != nil {
+	if err = cleanenv.ReadEnv(&cfg); err != nil {
 		log.Fatal("Error while getting config: ", err)
 		return nil
 	}
