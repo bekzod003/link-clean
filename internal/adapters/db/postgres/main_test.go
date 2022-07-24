eda70017a45f661f2756c67384a501d09990239e
bekzod003 <begiobm@gmail.com>
2022-07-24 15:35:26 +0500

    added some unit tests

---
+++ internal/adapters/db/postgres/main_test.go
@@ -0,0 +1,38 @@
+package postgres
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/bekzod003/link-clean/config"
+	"github.com/bekzod003/link-clean/pkg/database/client/postgresql"
+)
+
+func newClient() postgresql.Client {
+	cfg := config.GetConfig()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	client, err := postgresql.NewClient(
+		ctx, postgresql.ClientConfig{
+			Login:    cfg.PostgreSQL.User,
+			Password: cfg.PostgreSQL.Password,
+			Host:     cfg.PostgreSQL.Host,
+			Port:     cfg.PostgreSQL.Port,
+			DBName:   cfg.PostgreSQL.DBName,
+			PoolConfig: postgresql.PoolConfig{
+				MaxConns:                 cfg.PostgreSQL.PoolConfig.MaxConns,
+				MaxConnIdleMinutes:       cfg.PostgreSQL.PoolConfig.MaxConnIdleMinute,
+				MaxConnLifetimeMinutes:   cfg.PostgreSQL.PoolConfig.MaxConnLifetimeMinute,
+				HealthCheckPeriodMinutes: cfg.PostgreSQL.PoolConfig.HealthCheckPeriodMinute,
+			},
+		},
+	)
+	if err != nil {
+		log.Fatal("Error while getting new postgres client")
+	}
+
+	return client
+}
---
+++ internal/adapters/db/postgres/user_test.go
@@ -0,0 +1,58 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"github.com/bekzod003/link-clean/internal/domain/entities"
+	"testing"
+	"time"
+)
+
+// @TODO: create user with random values!
+func createUser(ctx context.Context, storage *userStorage) (int64, error) {
+	userID, err := storage.Create(
+		ctx,
+		&entities.User{
+			ID:        10,
+			Username:  "username",
+			FirstName: "firstname",
+			LastName:  "lastname",
+		},
+	)
+	return userID, err
+
+}
+
+func TestUserStorage_Create(t *testing.T) {
+	client := newClient()
+	storage := NewUserStorage(client)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	userID, err := createUser(ctx, storage)
+	if err != nil {
+		t.Error("Error while creating user:", err)
+	}
+	if userID == 0 {
+		t.Error("UserID is equal to 0!")
+	}
+}
+
+func TestUserStorage_Get(t *testing.T) {
+	client := newClient()
+	storage := NewUserStorage(client)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	res, err := storage.Get(ctx, 1)
+	if err != nil {
+		t.Error("Error while getting user by id:", err)
+	}
+	if res == nil {
+		t.Error("User is nil!")
+	}
+
+	fmt.Printf("Result: %+v", res)
+}
